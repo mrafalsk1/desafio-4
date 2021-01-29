eef165a7233fed41134e6be3e415fb0d27863464
Matheus Rafalski <matheus__rafalski@hotmail.com>
2021-01-29 16:37:12 -0300

    first commit

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,18 @@
 import { Router } from 'express';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const balance = transactionsRepository.getBalance();
+    const transactions = transactionsRepository.all();
+    return response.json({transactions,balance})
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +20,16 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const {title, value, type} = request.body
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository
+    )
+    const transaciton = createTransaction.execute({
+      title,
+      value,
+      type
+    });
+    return response.json(transaciton)
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
